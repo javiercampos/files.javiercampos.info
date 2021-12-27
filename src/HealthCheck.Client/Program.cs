11391633405795d6d3c18dca61ab21ec6baaa87c
Jcl <jcl@javiercampos.info>
2021-12-27 19:01:34 +0100

    Added custom smaller .NET healthcheck client, removed curl dependency on docker

---
+++ src/HealthCheck.Client/Program.cs
@@ -0,0 +1,14 @@
+﻿// Jcl: simple docker healthcheck tool
+// Created to remove the "curl" dependency on alpine
+var http = new HttpClient();
+try
+{
+    var result = await http.GetAsync("http://localhost:80/healthcheck");
+    result.EnsureSuccessStatusCode();
+}
+catch
+{
+    return 1;
+}
+
+return 0;
